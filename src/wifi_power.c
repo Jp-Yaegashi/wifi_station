33726bb08b631e4a33c80eba1702eff77d73fb23
Jp-Yaegashi <yae.dev.jp@gmail.com>
2025-12-24 19:07:57 +0900

    開発

--- src/wifi_power.c
+++ src/wifi_power.c
@@ -7,6 +7,7 @@
 
 
 /* DeviceTree から GPIO を取得 */
+/*
 static const struct gpio_dt_spec coex_status0 =
     GPIO_DT_SPEC_GET(DT_NODELABEL(coex_status0), gpios);
 
@@ -25,13 +26,21 @@ static const struct gpio_dt_spec sw_ctrl1 =
 static const struct gpio_dt_spec led_a =
     GPIO_DT_SPEC_GET(DT_NODELABEL(led_a), gpios);
 static const struct gpio_dt_spec led_b =
-    GPIO_DT_SPEC_GET(DT_NODELABEL(led_b), gpios);
+    GPIO_DT_SPEC_GET(DT_NODELABEL(led_b), gpios);*/
+
+
+static const struct gpio_dt_spec wifi_buck_en =
+    GPIO_DT_SPEC_GET(DT_NODELABEL(wifi_buck_en), gpios);
+
+static const struct gpio_dt_spec wifi_en =
+    GPIO_DT_SPEC_GET(DT_NODELABEL(wifi_en), gpios);
 
 /* 起動時に自動実行される */
 static int wifi_power_init(void)
 {
 
-	printk("### CoEx_bt called ###\n");
+	printk("### wifi_power_init ###\n");
+    /*
 
     if (!device_is_ready(coex_status0.port) ||
         !device_is_ready(coex_req.port) ||
@@ -46,7 +55,7 @@ static int wifi_power_init(void)
     gpio_pin_configure(coex_req.port, 11, GPIO_INPUT | GPIO_PULL_DOWN);
     gpio_pin_configure_dt(&coex_status0, GPIO_OUTPUT_INACTIVE); //OK
     
-   // gpio_pin_configure_dt(&coex_req, GPIO_OUTPUT_INACTIVE); //NG
+   
     
     gpio_pin_configure_dt(&coex_grant, GPIO_OUTPUT_INACTIVE); //OK
 
@@ -75,7 +84,30 @@ static int wifi_power_init(void)
     printk("raw=%d\n", gpio_pin_get_raw(coex_req.port, 11));
 
     
+*/
+if (!device_is_ready(wifi_buck_en.port) ||
+        !device_is_ready(wifi_en.port)) {
+        return -ENODEV;
+    }
 
+    gpio_pin_configure_dt(&wifi_buck_en, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure_dt(&wifi_en, GPIO_OUTPUT_INACTIVE);
+	k_msleep(60);
+    gpio_pin_set_dt(&wifi_buck_en, 0); /* BUCK_EN */
+    k_msleep(100);
+
+    printk("### wifi_en called ###\n");
+    gpio_pin_set_dt(&wifi_en, 0);      /* IOVDD */
+    k_msleep(100);
+
+    /* WM02C 推奨電源シーケンス */
+    printk("### wifi_buck_en called ###\n");
+    gpio_pin_set_dt(&wifi_buck_en, 1); /* BUCK_EN */
+    k_msleep(1000);
+
+    printk("### wifi_en called ###\n");
+    gpio_pin_set_dt(&wifi_en, 1);      /* IOVDD */
+    k_msleep(5000);
 
     return 0;
 }
